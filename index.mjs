f000e61fbad655f7bad77b343d9882e7864948c3
Younes HENNI <youn.henni@gmail.com>
2023-07-08 12:47:39 +0200

    Adds hashing

--- index.mjs
+++ index.mjs
@@ -1,5 +1,7 @@
+import { compareSync, genSaltSync, hashSync } from 'bcrypt';
 import express from 'express'
 import { readFile, writeFile } from 'fs/promises'
+const pepper = "PEPPER";
 
 const app = express();
 
@@ -50,15 +52,16 @@ app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     const users = await getUsers();
     const currentUser = users.find(({ username: foundUsername }) => foundUsername === username);
-    if (currentUser === undefined || currentUser?.password !== password) res.status(404).render('login', { error: true })
+    if (currentUser === undefined || hashSync(password, currentUser.salt + pepper) !== currentUser.password) res.status(404).render('login', { error: true })
     else res.status(200).redirect('/')
-
 })
 
 app.get('/register', (req, res) => res.status(200).sendFile('pages/register.html', { root: '.' }))
 app.post('/register', async (req, res) => {
-    await writeUser(req.body);
-    res.status(303).redirect('/login')
+    const salt = genSaltSync();
+    const hashedPassword = hashSync(req.body.password, salt + pepper);
+    writeUser({ ...req.body, password: hashedPassword, salt })
+
 })
 
 app.use('/static', express.static('static'))
