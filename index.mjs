28485adbed85eb46fd58a9699cbd649af0614eb9
Younes HENNI <youn.henni@gmail.com>
2023-07-10 18:39:25 +0200

    SPA TAble

--- index.mjs
+++ index.mjs
@@ -6,7 +6,6 @@ const pepper = "PEPPER";
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.json())
 
 async function getUsers() {
     let users = [];
@@ -35,7 +34,7 @@ async function writeUser(user) {
 let n = 0;
 
 
-app.all('/', async (req, res, next) => {
+app.all('/', express.urlencoded(), async (req, res, next) => {
     if (['POST', "GET"].includes(req.method)) {
         const users = await getUsers();
         if (req.method === "POST") {
@@ -48,7 +47,7 @@ app.all('/', async (req, res, next) => {
 })
 
 app.get('/login', (req, res) => res.status(200).render('login', { error: false, success: false }))
-app.post('/login', async (req, res) => {
+app.post('/login', express.json(), async (req, res) => {
     const { username, password } = req.body;
     const users = await getUsers();
     const currentUser = users.find(({ username: foundUsername }) => foundUsername === username);
@@ -57,7 +56,7 @@ app.post('/login', async (req, res) => {
 })
 
 app.get('/register', (req, res) => res.status(200).sendFile('pages/register.html', { root: '.' }))
-app.post('/register', async (req, res) => {
+app.post('/register', express.urlencoded(), async (req, res) => {
     const salt = genSaltSync();
     const hashedPassword = hashSync(req.body.password, salt + pepper);
     writeUser({ ...req.body, password: hashedPassword, salt })
@@ -68,6 +67,12 @@ app.get('/users', async (req, res) => {
     const users = await getUsers();
     res.status(200).json(users)
 });
+
+app.post('/user', express.json(), async (req, res) => {
+    const user = req.body;
+    await writeUser(user);
+    res.status(200).send("Success")
+})
 
 app.use('/static', express.static('static'))
 
--- static/index.js
+++ static/index.js
@@ -1,3 +1,30 @@
-fetch("http://109.123.250.96:3000/users")
+fetch("/users")
     .then(res => res.json())
-    .then(console.log)
+    .then(data => {
+        const tableHTML = data.map(({ nom, password, email }) => `<tr><td>${nom}</td><td>${email}</td><td>${password}</td></tr>`).join("");
+        document.querySelector("table > tbody").innerHTML = tableHTML;
+    })
+
+document.querySelector("form").addEventListener("submit", e => {
+    e.preventDefault();
+    const username = document.querySelector("#nom").value;
+    const password = document.querySelector("#password").value;
+    const email = document.querySelector("#email").value;
+    fetch("/user", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password, email })
+    })
+        .then(res => {
+            if (res.status === 200) {
+                fetch("/users")
+                    .then(res => res.json())
+                    .then(data => {
+                        const tableHTML = data.map(({ nom, password, email }) => `<tr><td>${nom}</td><td>${email}</td><td>${password}</td></tr>`).join("");
+                        document.querySelector("table > tbody").innerHTML = tableHTML;
+                    })
+            }
+        })
+});
